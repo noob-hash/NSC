b3aebd1816b2873f4a6b1b96a5848afe1a809de5
Subin <subinsedhai002@gmail.com>
2023-09-16 11:02:45 +0545

    [Added authentication procedure]

--- MadeInNepal/src/java/Services/UserServices.java
+++ MadeInNepal/src/java/Services/UserServices.java
@@ -4,10 +4,34 @@
  */
 package Services;
 
+import DBConnection.DatabaseConn;
+import Models.Users;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Subin
  */
 public class UserServices {
-    
+    public Users GetUser(int id) {
+        Users user = null;
+        try {
+            Connection con = new DatabaseConn().ConnectionEstablishment();
+            String statement = "Select * from user where user_id = ? and deleted = '0'";
+            PreparedStatement ps = con.prepareStatement(statement);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                user = new Users(rs.getInt("user_id"),rs.getString("username"),  Users.Role.valueOf(rs.getString("user_type")));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserServices.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return user;
+    }
 }
